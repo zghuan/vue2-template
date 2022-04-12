d9b57caca9c2e079ef30f0f884ba516bf7ba0ad4
zengguanhuan <785774770@qq.com>
2022-04-12 18:41:39 +0800

    add promise 50%

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,12 +2,33 @@
   <div>{{CONFIG.zgh}}</div>
 </template>
 <script>
+import { PromiseAlwa } from '../plugins/promise'
 export default {
   name: 'Home',
   data () {
     return {
       CONFIG
     }
+  },
+  created () {
+    new PromiseAlwa((resolve, reject) => {
+      console.log('开始执行executor')
+      resolve('成功')
+      // reject('失败')
+      // setTimeout(() => {
+      //   resolve('成功1')
+      // }, 1000)
+      // setTimeout(() => {
+      //   reject('失败1')
+      // }, 2000)
+    }).then(res => {
+      console.log(res, 'then1')
+      return 'alwa'
+    }).then(res => {
+      console.log(res, 'then2')
+    }).catch(err => {
+      console.log(err, 'catch')
+    })
   }
 }
 </script>
