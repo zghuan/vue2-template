59d4e67d33362e311d77f40bf0db13dbb466a51a
zengguanhuan <785774770@qq.com>
2022-05-10 17:59:40 +0800

    completion md-loader

--- loader/md-loader.js
+++ loader/md-loader.js
@@ -1,10 +1,14 @@
 // 一个简单的loader实例，md转成console打印出来
 const { marked } = require('marked')
 module.exports = function (source) {
-  const html = marked(source)
-  const str = html.match(/>(\S*)</)[1]
-  return `module.exports = console.info.apply(console.info, [
-    '%c ${str} ',
-    'padding: 1px; border-radius: 3px 0 0 3px; color: #fff; background: #606060;'
-  ])`
+  const html = marked(source).replace(/[\r\n]/g, '')
+  const str = html.match(/(?<=>).\S*?(?=<)/gm)
+  let res = ''
+  str.forEach(item => {
+    res += `console.info.apply(console.info, [
+      '%c ${item} ',
+      'padding: 1px; border-radius: 3px; color: #fff; background: orange'
+    ])\n`
+  })
+  return res
 }
